faa59d8a1fdd1027c4943b9b8d958ac5e20167ff
pyurov <pyurov@techsquare.com>
2026-03-17 10:25:31 -0400

    adding thunder strike calc but in c

---
+++ c-practice/c-functions/thunder_strike.c
@@ -0,0 +1,24 @@
+/*
+* Author: Philip Y.
+*/
+#include <stdio.h>
+
+void calculateDistance() {
+
+  int time; // declare var
+  printf("Enter time in seconds between sound of thunder and flash of lightning: ");
+  scanf("%d", &time); // getting input from STDIN
+
+  // error handling
+  if (!(time > 0)) {
+    printf("Invalid input\n");
+  } else {
+    int distance = time / 5; // simple arithmetic to get storm distance
+    printf("You are %d miles away from the storm\n", distance);
+  }
+}
+
+int main() {
+  calculateDistance(); // call the function
+  return 0;
+}
