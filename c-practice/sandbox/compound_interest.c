065acf3bbcdbf5aefed97ed59cf2c294cbc2532a
pyurov <pyurov@techsquare.com>
2026-03-17 14:21:01 -0400

    compound interest in c

---
+++ c-practice/sandbox/compound_interest.c
@@ -0,0 +1,30 @@
+/*
+* Author: Philip Y.
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+  float principalInvestment;
+  float interestRate;
+  float timesAYear;
+  int years;
+  float futureValue;
+
+  printf("Enter starting principal: ");
+  scanf("%f", &principalInvestment);
+  printf("Enter annual interest rate: ");
+  scanf("%f", &interestRate);
+  interestRate = interestRate / 100;
+  printf("How many times a year is the interest compounded: ");
+  scanf("%f", &timesAYear);
+  printf("How many years will the account earn interest: ");
+  scanf("%d", &years);
+
+  // formula FV=PV(1+r/m)**mt
+  futureValue = principalInvestment * pow(1 + interestRate / timesAYear, timesAYear * years);
+  printf("At the end of %d years you will have $%.2f\n", years, futureValue);
+
+  return 0;
+}
